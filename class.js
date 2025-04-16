89dc307dcb0005218f688264cedd38b2ffdf9aab
Saurabh_Yadav <saurabhyadavv1305@gmail.com>
2025-04-16 17:57:56 +0530

    class

---
+++ class.js
@@ -0,0 +1,96 @@
+//             CLASS
+// this development reduced 
+
+// class ClassName{
+//   constructor(prop1,prop2){
+//     this.prop1=prop1;
+//     this.prop2=prop2;
+
+//   }
+// }
+
+// let obj=new ClassName("arg1","arg2");
+// console.log(obj.prop1);
+
+
+// this keyword refers to the object it belongs 
+
+
+
+
+
+
+
+// class Dog{
+
+//   constructor(dogName , weight ,colour,breed){
+//     this.dogName=dogName;
+//     this.weight=weight;
+//     this.colour=colour;
+//     this.breed=breed;
+//   }
+// }
+
+// let dog =new Dog("JavaScript",2.4,"brown","chihuahua");
+
+// console.log(dog.dogName , "is a" ,);
+
+
+
+
+
+// CLAss     it is a convention to start a class name with a capital letter
+
+// class Person{
+//   constructor(firstname,lastname){
+//     this.firstname=firstname;
+//     this.lastname=lastname;
+
+//   }
+// }
+// let p=new Person("Maaike");
+
+// console.log("hi",p.firstname,p.lastname);
+
+
+
+
+
+//  default argument
+// class Person{
+//   constructor(firstname,lastname="doe"){
+//     this.firstname=firstname;
+//     this.lastname=lastname;
+
+//   }
+// }
+
+// let p=new Person("Maaike");
+
+// console.log("hi",p.firstname,p.lastname);
+
+
+
+
+
+//          Method
+
+// use dont use function keyword
+
+// class Person{
+//     constructor(firstname,lastname="doe"){
+//       this.firstname=firstname;
+//       this.lastname=lastname;
+  
+//     }
+  
+//     greet (){
+//       console.log("Hi there !",this.firstname);
+//     }
+//   }
+  
+  
+//   let p=new Person("Maaike", "van putten");
+//   p.greet();
+        
+    
