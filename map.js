26c8edd320cf229bb2d390789b127f2a8276fec9
Saurabh_Yadav <saurabhyadavv1305@gmail.com>
2025-04-29 12:58:30 +0530

    Array Destructuring

--- map.js
+++ map.js
@@ -13,7 +13,7 @@
 // fruits.set("oranges", 200);
 // console.log(fruits);
 
-// JS MAP METHODS
+// JS MAP METHODS 
 
 // const fruits=new Map([
 //     ["apples",500],
@@ -100,7 +100,7 @@
 // order of properties does not matter
 
 
-// Alies creation
+// Alias creation
 
 
 // const person={
@@ -111,3 +111,79 @@
 //     //  destructuring
 //     let{lastname:name}=person;
 //     console.log(person);
+  
+
+// string destructuring
+
+// let name="GurukulTheSchool"
+// let[a1,a2,a3,a4,a5]=name;
+// console.log(a1,a2,a3,a4,a5);
+
+
+// // Array Destructuring 
+
+// array constructing
+// const fruits=["bananas","oranges","Apples","mangoes"];
+
+
+// destructing
+
+// let[fruit1, ,,fruit2]=fruits;
+
+// console.log(fruit1);
+// console.log(fruit2);
+
+
+// const fruits=["bananas","oranges","Apples","mangoes"];
+
+
+// let{[0]:fruit1,[1]:fruit2}=fruits;
+
+// console.log(fruit1);
+// console.log(fruit2);
+
+
+// const numbers=[10,20,30,40,50,60,70,80,90];
+// const[a,b,...rest]=numbers
+// console.log(a);
+
+
+// swap two variable using destructuring
+
+// let firstname="John";
+// let lastname="Doe";
+// [firstname,lastname]=[lastname,firstname];
+// console.log(lastname);
+
+// EXPONENTIAL OPERATOR
+
+// let x=5,a=3;
+// let z=x**2;
+// console.log(z);
+// let y=Math.pow(x,a);
+// console.log(y)
+
+
+// const fruit=["bananas","oranges","Apples","mangoes"];
+// fruit.includes("mangoes");
+// console.log("bananas");
+
+
+// trailing commas
+
+
+// const arr=[
+//     "one",
+//     "two",
+//     "three",,
+// ];
+// console.log(arr.length);
+
+
+// const person={
+//     firstname:"john",
+//     lastname:"Doe",
+//     age:20,
+//     ,
+//     ,
+// }
